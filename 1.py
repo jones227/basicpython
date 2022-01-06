1d0d69648767c2d2afe07e354cec63e676826f06
jones227 <83818236+jones227@users.noreply.github.com>
2022-01-06 09:10:51 +0100

    lister og while løkke og en liten if

--- 1.py
+++ 1.py
@@ -1,5 +1,41 @@
 print('hello world')
 
 x = 'hvordan har du det'
+y = 10
+z = 'bra'
+b = True
+lis = []
+
+snipers = ['weskeru', 'vadikus', 'igor']
+streamers =['forsen', 'nymn', 'xqcow']
+
+while b:
+    # print(y)
+    y = y + 1
+    lis.append(y)
+    if y > 100:
+        # break
+        b = False
+lis[5] = 'donk'
+
+zulul = []
+zulul.append(snipers)
+zulul.append(streamers)
+
+print(zulul)
+
+print(len(lis))
+
+print(lis)
+print(y)
+
+
+
+
+
+print(type(y))
+print(type(x))
+print(type(b))
 
 print(x)
+print(x + z)
