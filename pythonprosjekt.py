9574f325cbc5f6c9b909fa54943e32194e6fcfd8
jones <83818236+jones227@users.noreply.github.com>
2022-01-27 01:37:28 +0100

    Add files via upload

--- pythonprosjekt.py
+++ pythonprosjekt.py
@@ -1,26 +1,8 @@
 import calendar
-from Tkinter import Tk, Entry
+from tkinter import simpledialog
 
-# declare the window
-window = Tk()
-# set window title
-window.title("Python GUI App")
-# set window width and height
-window.configure(width=500, height=300)
-# set window background color
-window.configure(bg='lightgray')
-window.mainloop()
+yy = simpledialog.askinteger("Input", "type a year")
+mm = simpledialog.askinteger("Input", "type a month")
+day = simpledialog.askinteger("Input", "type a day")
 
-tk = Tk()
-inputBox = Entry(tk, bd=5)
-
-#to read your box
-inputBox.get()
-yy = int(input('write year'))
-mm = int(input('write month'))
-day = int(input('write day'))
-
-
-
-
-print(calendar.weekday(yy, mm, day))
+print((calendar.day_name[calendar.weekday(yy, mm, day)].upper()))
