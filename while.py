c5a9f3898c7d1e081e01dab15e6ac20edc8a063c
jones227 <83818236+jones227@users.noreply.github.com>
2022-01-31 11:32:32 +0100

    while

---
+++ while.py
@@ -0,0 +1,16 @@
+forsen = "happy"
+frozen = 0
+
+def lul_e():
+    while forsen == "happy":
+        print(forsen)
+        break
+
+
+while frozen < 10:
+    print(frozen)
+    frozen = frozen + 1
+    if frozen == 10:
+        break
+
+lul_e()
